671bc82251681acd86ef3e1dc4ddb7e8f85eac20
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-10-15 13:35:40 +0300

    task02 solutons

---
+++ lectures/05-functions-and-code-organization/homework/task02/parameters-and-returns.js
@@ -0,0 +1,29 @@
+import { formatExecutionTime } from "../task01/functions-declarations-basics.js";
+
+function isValidEmail(email) {
+  const hasAtSymbol = email.includes("@");
+  const hasDotSymbol = email.includes(".");
+  const isNotEmpty = email.length > 0;
+
+  return hasAtSymbol && hasDotSymbol && isNotEmpty;
+}
+
+console.log("isValid with valid email:",isValidEmail("test@example.com"));
+console.log("isValid with invalid email:",isValidEmail("test@examplecom"));
+console.log("is valid with empty string email",isValidEmail(""));
+
+function formatDuration(miliseconds) {
+  return formatExecutionTime(miliseconds);
+}
+
+console.log("formatDuration above 1000:",formatDuration(1999));
+console.log("formatDuration below 1000:",formatDuration(999));
+
+function generateTestEmail(baseName, domain = "testcompany.com") {
+  return `${baseName}@${domain}`;
+}
+
+console.log("generateTestEmail without domain:",generateTestEmail("lubo"));
+console.log("generateTestEmail with domain:",generateTestEmail("lubo", "gmail.com"));
+
+
